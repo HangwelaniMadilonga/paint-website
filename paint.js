d6ee659b417807ab3ec7cee38b63d44f129936eb
Hangwelani Madilonga <hangwelani3@gmail.com>
2023-10-29 21:05:59 +0200

    Reworked listeners using html5 canva.

--- paint.js
+++ paint.js
@@ -1,42 +1,49 @@
 let isBrush = true;
 let isEraser = false;
-
 let isDrawing = false;
 
-let currentColor = "boo";
+let currentColor = "black"; // Default to black. You can change it later.
 let currentTool = null;
 
 let buttons = document.querySelectorAll('.colors');
 let brush = document.querySelector('#brush-icon');
 let eraser = document.querySelector('#eraser-icon');
-let canvasArea = document.querySelector('#canvas');
+let canvas = document.querySelector('#canvas-section');
+let ctx = canvas.getContext('2d'); // Get the 2D drawing context
 let drawTools = document.querySelectorAll('.tools');
 
+canvas.addEventListener('mousedown', function () {
+    isDrawing = true;
+    draw(event); // Start drawing immediately upon click
+});
+
+canvas.addEventListener('mouseup', function () {
+    isDrawing = false;
+    ctx.beginPath(); // End the current drawing path
+});
+
+canvas.addEventListener('mousemove', draw);
 
 brush.addEventListener('click', function () {
     currentTool = "brush";
-
-})
+    ctx.globalCompositeOperation = "source-over"; // Default mode for painting
+});
 
 eraser.addEventListener('click', function () {
     currentTool = "eraser";
-
-})
+    ctx.globalCompositeOperation = "destination-out"; // Use this to erase
+});
 
 for (let button of buttons) {
     button.addEventListener('click', function () {
         selectColor(button);
-
     })
 }
 
 for (let tool of drawTools) {
     tool.addEventListener('click', function () {
         selectTool(tool);
-
     })
-
-
 }
 
 let selectColor = (button) => {
@@ -45,6 +52,7 @@ let selectColor = (button) => {
     }
 
     currentColor = window.getComputedStyle(button).backgroundColor;
+    ctx.strokeStyle = currentColor; // Set the stroke color for the canvas
     button.classList.add('enlarge-color');
 };
 
@@ -52,20 +60,16 @@ let selectTool = (tool) => {
     for (let tool of drawTools) {
         tool.classList.remove('enlarge-tool');
     }
-
-
-    // drawTool = drawTool.style.backgroundColor;
     tool.classList.add('enlarge-tool');
-
-    // console.log(currentColor);
-
 };
 
-let paint = () => {
+function draw(event) {
+    if (!isDrawing) return;
+    ctx.lineWidth = 5; // You can change this for a thicker/thinner line
+    ctx.lineCap = "round"; // Makes the line edges round
 
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(event.offsetX, event.offsetY);
 }
-
-let erase = () => {
-
-}
-
